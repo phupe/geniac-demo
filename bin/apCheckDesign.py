a480e2643cf2e0f3b089c8a4df2a4481be73fe2d
Philippe Hupé <philippe.hupe@curie.fr>
2020-10-28 23:35:02 +0100

    [MODIF] header license (Issue #14)

--- bin/apCheckDesign.py
+++ bin/apCheckDesign.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-##############################################################################
-# Copyright Institut Curie 2020
-# This software is a computer program whose purpose is to analyze high-throughput sequencing data.
-# You can use, modify and/ or redistribute the software under the terms of license (see the LICENSE file for more details).
-# The software is distributed in the hope that it will be useful, but "AS IS" WITHOUT ANY WARRANTY OF ANY KIND.
-# Users are therefore encouraged to test the software's suitability as regards their requirements in conditions enabling the security of their systems and/or data.
-# The fact that you are presently reading this means that you have had knowledge of the license and that you accept its terms.
-##############################################################################
+#############################################################################################
+# Copyright Institut Curie 2020                                                             #
+#                                                                                           #
+# This software is a computer program whose purpose                                         #
+# is to analyze high-throughput sequencing data.                                            #
+# You can use, modify and/ or redistribute the software under                               #
+# the terms of license (see the LICENSE file for more details).                             #
+# The software is distributed in the hope that it will be useful,                           #
+# but "AS IS" WITHOUT ANY WARRANTY OF ANY KIND.                                             #
+# Users are therefore encouraged to test the software's suitabilityas regards               #
+# their requirements in conditions enabling the security of their systems and/or data.      #
+# The fact that you are presently reading this means that                                   #
+# you have had knowledge of the license and that you accept its terms.                      #
+#############################################################################################
 
 import argparse
 import csv
--- main.nf
+++ main.nf
@@ -2,11 +2,17 @@
 
 /*
 Copyright Institut Curie 2020
-This software is a computer program whose purpose is to analyze high-throughput sequencing data.
-You can use, modify and/ or redistribute the software under the terms of license (see the LICENSE file for more details).
-The software is distributed in the hope that it will be useful, but "AS IS" WITHOUT ANY WARRANTY OF ANY KIND.
-Users are therefore encouraged to test the software's suitability as regards their requirements in conditions enabling the security of their systems and/or data.
-The fact that you are presently reading this means that you have had knowledge of the license and that you accept its terms.
+
+This software is a computer program whose purpose is to
+analyze high-throughput sequencing data.
+You can use, modify and/ or redistribute the software under the terms
+of license (see the LICENSE file for more details).
+The software is distributed in the hope that it will be useful,
+but "AS IS" WITHOUT ANY WARRANTY OF ANY KIND.
+Users are therefore encouraged to test the software's suitability as regards
+their requirements in conditions enabling the security of their systems and/or data.
+The fact that you are presently reading this means that you have had knowledge
+of the license and that you accept its terms.
 
 This script is based on the nf-core guidelines. See https://nf-co.re/ for more information
 */
@@ -407,6 +413,10 @@ process getSoftwareVersions{
   """
 }
 
+/********************
+ * Workflow summary *
+ ********************/
+
 process workflowSummaryMqc {
   when:
   !params.skipMultiQC
@@ -464,7 +474,7 @@ process multiqc {
   designOpts = params.design ? "-d ${params.design}" : ""
   modulesList = "-m custom_content -m fastqc"
   """
-  apMqcHeader.py --splan ${splan} --name "PIPELINE" --version "${workflow.manifest.version}" ${metadataOpts} > multiqc-config-header.yaml
+  apMqcHeader.py --splan ${splan} --name "geniac demo" --version "${workflow.manifest.version}" ${metadataOpts} > multiqc-config-header.yaml
   multiqc . -f $rtitle $rfilename -c multiqc-config-header.yaml -c $multiqcConfig $modulesList
   """
 }
